a2ae2e6d6efca3ce490eb99e669f5ae34d9a6b64
VikingDDM <chrisdulin4ever@gmail.com>
2022-08-27 18:32:23 +0300

    Missed comma in translation for vue template

--- src/interfaces/plugin-options.ts
+++
@@ -1,8 +0,0 @@
-import { OptionsInterface } from './options'
-
-/**
- * The Interface that is responsible for the Options provided.
- */
-export interface PluginOptionsInterface extends OptionsInterface {
-  shared?: boolean
-}
